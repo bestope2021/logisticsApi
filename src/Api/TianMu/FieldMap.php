85c19547ac8f78dec172db848280dad1cc9c998e
ggx <ggx9110@163.com>
2021-05-24 18:08:20 +0800

    bug fix

--- src/Api/TianMu/FieldMap.php
+++ src/Api/TianMu/FieldMap.php
@@ -55,6 +55,7 @@ class FieldMap extends LsSdkFieldMapAbstract implements LsSdkFieldMapInterface
             'lable_file',// 面单路径URL
             'label_path_plat',// 平台路径
             'lable_content_type',// 面单类型
+            'extended',// 扩展参数
         ];
 
         return self::getFieldMap(self::getPackagesLabelFields(), $field);
