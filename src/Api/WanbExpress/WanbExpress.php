afdd6ccef76450ee22b308eeb57afa75e3cc8ffb
ggx <ggx9110@163.com>
2021-06-25 10:47:20 +0800

    bug fix

--- src/Api/WanbExpress/WanbExpress.php
+++ src/Api/WanbExpress/WanbExpress.php
@@ -191,10 +191,10 @@ class WanbExpress extends LogisticsAbstract implements BaseLogisticsInterface, T
                     'Address' => $item['senderFullAddress'] ?? '',// N:发件人完整地址Length <= 200
                     'Postcode' => $item['senderPostCode'] ?? '',// N:发件人邮编Length <= 32
                     'ContactInfo' => $item['senderPhone'] ?? ($item['senderPhone'] ?? ($item['senderEmail'] ?? '')), // N:联系信息(邮箱 或者 电话)
-                    'Taxations' => [
+                    'Taxations' => [[
                         'TaxType' => 'IOSS',//IOSS: 欧盟2021税改后，进入欧盟货物需要提供 IOSS
                         'Number' => $item['iossNumber'] ?? '',// 欧盟税号（ioss税号）
-                    ]
+                    ]]
                 ],
                 'ShippingAddress' => [// 发件人信息
                     'Contacter' => $item['recipientName'] ?? '',// Y:收件人姓名
