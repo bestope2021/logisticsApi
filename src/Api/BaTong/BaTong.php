cb331fb137fa09b57eaa37a5975b9fa1a7e5de7d
michael <michael@git.bestope.tech>
2022-01-10 18:26:56 +0800

    更新巴通下单单价及重量精度问题

--- src/Api/BaTong/BaTong.php
+++ src/Api/BaTong/BaTong.php
@@ -111,8 +111,8 @@ class BaTong extends LogisticsAbstract implements BaseLogisticsInterface, TrackL
                     'invoice_cnname' => $value['declareCnName'] ?? '',// N:申报中文名称Length <= 50
                     'invoice_quantity' => (int)($value['quantity'] ?? ''),// Y:产品数量;数值必须为正整数
                     'unit_code' => 'PCE', //N:单位  MTR：米  PCE：件 SET：套 默认PCE
-                    'invoice_weight' => $value['declareWeight'] ?? '',// Y:总量;Length <= 50 KG
-                    'invoice_unitcharge' => (float)($value['declarePrice'] ?? ''), //Y:单价
+                    'invoice_weight' => empty($value['declareWeight']) ? '0.000' : round($value['declareWeight'], 3),// Y:总量;Length <= 50 KG
+                    'invoice_unitcharge' => (empty($value['declarePrice']) ? '0.00' : round($value['declarePrice'], 2)), //Y:单价
                     'invoice_currencycode' => $value['currencyCode'] ?? 'USD',// , //申报币种，不传值默认为USD(美元)；USD-美元,AUD-澳元
                     'hs_code' => $value['hsCode'] ?? '',// N:海关编码
                     'invoice_note' => '', //配货信息
